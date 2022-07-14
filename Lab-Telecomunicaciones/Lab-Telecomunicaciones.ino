50522edd6a43cf34c42f36f20f5ba7733ea9f78a
RenzoVigiani <vigiani.renzo.l@gmail.com>
2022-07-14 11:22:34 -0300

    Actualización programa y readme Se realizó cambio de protocolo y ejecución

--- Lab-Telecomunicaciones/Lab-Telecomunicaciones.ino
+++ Lab-Telecomunicaciones/Lab-Telecomunicaciones.ino
@@ -1,213 +1,219 @@
-#include <ArduinoJson.h>
-#include <UIPEthernet.h>
-#include <Servo.h>
+// Defino las librerias a utilizar
+#include <ArduinoJson.h> // Para el manejo y conversión de JSON a vartiables
+#include <UIPEthernet.h> // Para el manejo del Shield Ethernet
+#include <Servo.h> // Para el manejo de los Servos.
+//----------------------------------------------//
+// Defino el servidor y el Puerto
+#define server_port 22 // 80
+EthernetServer server = EthernetServer(server_port);
+//----------------------------------------------//
+// Defino variables para Json
+#define const_mje 256
+#define const_valores 236
+//----------------------------------------------//
+//------- Defino mensajes de error predeterminado
+// una variable error int
+uint8_t Errores = 0;
+// 0 - Sin errores
+// 1 - Error limites de angulo de azimut.
+// 2 - Error limites de angulo de elevación.
+// 3 - Laboratorio incorrecto.
+//----------------------------------------------//
+//------- Defino variables globales
+// Nombres para los pines GPIO
+// Leds
+  #define Led_aux1 10 // Indicador aux 1
+  #define Led_aux2 11 // Indicador aux 2
+  #define Led_aux3 12 // Indicador aux 3
+  #define Led_aux4 13 // Indicador aux 4
+  #define Led_S1 2  // Indicador Servo 1
+  #define Led_S2 3 // Indicador Servo 2
+// Servos
+  #define azimut_pin 4  // Servo azimut
+  #define elevacion_pin 5  // Servo elevación
+// Declaramos la variable para controlar el servo
+  Servo servo_azimut;
+  Servo servo_elevacion;
 
-#define const_status 150
-#define const_instruc 130
 
-EthernetServer server = EthernetServer(22);
 //////// VAriables de json //////////////
 // Estado
-int num_Lab=0;
-bool subLab=true;
-bool iniLab=true;
+  int num_Lab=0;
+  bool subLab=true;
+  bool iniLab=true;
 // Analogico
-int variable_0=0;
-int variable_1=0;
-int variable_2=0;
-int variable_3=0;
+  int variable_0=0;
+  int variable_1=0;
+//--- Variables auxiliares ---//
+  int ang_azimut_act; //Angulos actuales de cada servo
+  int ang_elevacion_act;
+  bool bandera_rep=0; // bandera para limitar la cantidad de repeticiones de mensaje de lab incorrecto 
 
-////////////// Funciones  ////////////////////
-void wifi_24_ghz(int azimut, int elevacion);
-void rad_def_soft(bool iniLab, int intensidad_max, int intensidad_min, int modulacion, int codificacion);
-//-------------------//
-void get_json(EthernetClient client);
-void post_json(char instrucciones[const_instruc], EthernetClient client);
+// Constantes auxiliares
+  const int limite_min_azimut=0; // Indica el minimo valor que puede tomar distancia
+  const int limite_max_azimut=180; // Indica el minimo valor que puede tomar distancia
+  const int limite_min_elevacion=0; // indica el minimo valor que puede tomar distancia.
+  const int limite_max_elevacion=90; // indica el minimo valor que puede tomar distancia.
 
-// Declaramos la variable para controlar el servo
-Servo servo_azimut;
-Servo servo_elevacion;
-
-void setup() 
-{
-  uint8_t mac[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-  IPAddress myIP(172,20,5,140);
-  // Initialize serial port
-  Serial.begin(9600);
-  while (!Serial) continue;
-  // Initialize Ethernet libary
-  Ethernet.begin(mac,myIP);  
-  // Start to listen
-  server.begin();
+void setup(){
+  uint8_t myMAC[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // defino mac del dispositivo.
+  IPAddress myIP(192,168,1,108); // 172.20.5.140 Defino IP Address del dispositivo. 
+  Serial.begin(115200); // Inicializo Puerto serial 0 
+  while (!Serial) continue; 
+  Ethernet.begin(myMAC,myIP);  // Inicializo libreria Ethernet
+  server.begin(); // Start to listen
   Serial.println(F("Server is ready."));
   Serial.print(F("Please connect to http://"));
   Serial.println(Ethernet.localIP());
+  Serial.println("Port:" + (String)server_port);
+// ------- Defino GPIO MODE (INPUT/OUTPUT)--------  //  
+// Leds
+  pinMode(Led_aux1, OUTPUT); // Led Motor 1
+  pinMode(Led_aux2, OUTPUT); // Led Motor 2
+  pinMode(Led_aux3, OUTPUT); // Led Motor 3
+  pinMode(Led_aux4, OUTPUT);// Led Aux
+  pinMode(Led_S1, OUTPUT); // Led Servo 1
+  pinMode(Led_S2, OUTPUT); // Led Servo 2
+// Servos
+  servo_azimut.attach(azimut_pin);
+  servo_elevacion.attach(elevacion_pin);
+//------ Definir estados iniciales ------//
+}
 
-// Iniciamos el pin de cada servo
-  servo_azimut.attach(9);
-  servo_elevacion.attach(3);
-  }
-
-void loop() 
-{
-
+void loop(){
 //////////// Strings de comunicación /////////////
-char status[const_status] = {0};
-char instrucciones[const_instruc] = {0};
+char Mensaje_recibido[const_mje] = {0}; // Mensaje recibido por ethernet. (Comando + JSON) 
+char valores_recibidos[const_valores] = {0}; // JSON recibido.  // Wait for an incomming connection
+EthernetClient client = server.available(); 
+  if(client){ // Si tengo un cliente conectado
+    while (client.available()){ 
+      if(bandera_rep==1)bandera_rep=0; //reinicio bandera de repetición cuando tengo un mje nuevo.
+      Serial.println("New Command");
+      client.readBytesUntil('\r', Mensaje_recibido, sizeof(Mensaje_recibido)); // Tomo el mensaje recibido.
+      strncpy(valores_recibidos,&Mensaje_recibido[15],(sizeof(Mensaje_recibido)-15)); 
+      Serial.print("Mensaje Recibido: ");
+      Serial.println(Mensaje_recibido);   
+  //      Serial.print("Json_Recibido: ");
+  //      Serial.println(valores_recibidos);   
+      //------ GET ----- //
+      if (strstr(Mensaje_recibido, "GET /HTTP/1.1") != NULL) { // Compruebo si llega un GET, respondo valores actuales
+        StaticJsonDocument<256> doc;     
+        JsonArray Estado = doc.createNestedArray("Estado");
+        Estado.add(num_Lab);
+        Estado.add(subLab);
+        Estado.add(iniLab);
 
-  // Wait for an incomming connection
-  EthernetClient client = server.available(); 
-  // Do we have a client?
-  if (!client) return;
-  Serial.println();
-  Serial.println(F("New client"));
-  // Read the request (we ignore the content in this example)
-  while (client.available()) 
-  {
-    client.readBytesUntil('\r', status, sizeof(status));
-    Serial.println("status:");
-    Serial.println(status);
-//obtengo las instrucciones del formato json
-    strncpy(instrucciones,&status[15],(sizeof(status)-15));
-  //------ GET ----- //
-    if (strstr(status, "GET / HTTP/1.1") != NULL) 
-    {
-      get_json(client);
+        JsonArray Analogicos = doc.createNestedArray("Analogicos");
+        Analogicos.add(ang_azimut_act);
+        Analogicos.add(ang_elevacion_act);
+
+        JsonArray Error = doc.createNestedArray("Error");  
+        Error.add(Errores);
+
+        Serial.print(F("Sending: "));
+        serializeJson(doc, Serial);
+        Serial.println();
+    // Write response headers
+        client.println(F("HTTP/1.0 200 OK"));
+        client.println(F("Content-Type: application/json"));
+    // client.println(F("Connection: close"));
+        client.print(F("Content-Length: "));
+        client.println(measureJsonPretty(doc));
+        client.println();
+    // Write JSON document
+        serializeJsonPretty(doc, client);
+    // Disconnect
+        client.println(F("Get terminado"));
+    //      client.stop();
+      }
+      //------- POST -----//      
+      if (strstr(Mensaje_recibido, "POST /HTTP/1.1") !=NULL) { // Compruebo si llega un POST y respondo, habilito banderas.
+        Serial.println("Solicitud de escritura recibida");        
+        client.println();
+        client.println(F("HTTP/1.1 200 OK"));
+        client.println();
+        StaticJsonDocument<256> doc; // Creo un doc de json
+        DeserializationError error = deserializeJson(doc, valores_recibidos); // Deserializo
+        if (error) // Analizo posibles errores.
+        {
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.f_str());
+          return;
+        }
+
+        JsonArray Estado = doc["Estado"];
+        num_Lab = Estado[0]; // 0 [Sist Dig], 1 [Sist Control], 2[Telecomunicaciones], 3[Fisica]
+        subLab = Estado[1]; // 1 [SubLab 1], 0 [SubLab 2]
+        iniLab = Estado[2]; // 1 [Inicia Experimento], 0 [Finaliza Experimento]
+
+        if(num_Lab==2) // Control de numero de lab.
+        {
+          JsonArray Analogico = doc["Analogico"];
+          variable_0 = Analogico[0];
+          variable_1 = Analogico[1];
+        //client.stop();
+        }
+      }
     }
-  //------- POST -----//      
-    if (strstr(status, "POST / HTTP/1.1") !=NULL) 
-    {
-      post_json(instrucciones, client);
-    }
+  }
+  else{ // Si no está el cliente enviando algo, sigo haciendo lo que corresponde.
+      Control();
   }
 }
 
-void get_json(EthernetClient client)
-{
-  StaticJsonDocument<128> doc;     
-  JsonArray Estado = doc.createNestedArray("Estado");
-  Estado.add(num_Lab);
-  Estado.add(subLab);
-  Estado.add(iniLab);
-
-  JsonArray Analogico = doc.createNestedArray("Analogico");
-  Analogico.add(variable_0);
-  Analogico.add(variable_1);
-  Analogico.add(variable_2);
-  Analogico.add(variable_3);
-
-  Serial.print(F("Sending: "));
-  serializeJson(doc, Serial);
-  Serial.println();
-// Write response headers
-  client.println(F("HTTP/1.0 200 OK"));
-  client.println(F("Content-Type: application/json"));
-// client.println(F("Connection: close"));
-  client.print(F("Content-Length: "));
-  client.println(measureJsonPretty(doc));
-  client.println(); 
-// Write JSON document
-  serializeJsonPretty(doc, client);
-// Disconnect
-// client.stop();  
-}
-
-void post_json(char instrucciones[const_instruc], EthernetClient client)
-{
-    Serial.println("Solicitud de escritura recibida");
-    client.println(F("HTTP/1.1 200 OK"));
-    client.println();
-    StaticJsonDocument<128> doc;
-    // Deserializo
-    DeserializationError error = deserializeJson(doc, instrucciones);
-    
-    if (error) 
-    {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.f_str());
-      return;
-    }
-    
-    JsonArray Estado = doc["Estado"];
-    num_Lab = Estado[0]; // 0 [Sist Dig], 1 [Sist Control], 2[Telecomunicaciones], 3[Fisica]
-    subLab = Estado[1]; // true[SubLab 1], false [SubLab 2]
-    iniLab = Estado[2]; // true[Inicia Experimento], false[Finaliza Experimento]
-
-    JsonArray Analogico = doc["Analogico"];
-    variable_0 = Analogico[0]; // 
-    variable_1 = Analogico[1]; // 
-    variable_2 = Analogico[2]; // 
-    variable_3 = Analogico[3]; // 
-
-  if(num_Lab==2)
-  {
-    if (subLab and iniLab)
-    {
+/**
+ * @brief Funcion utilizada para el control de laboratorios. "Se analiza las variables de entrada y se ejecuta lo necesario"
+ * 
+ */
+void Control(){
+  if(num_Lab==2){
+    if (subLab and iniLab){
       Serial.println("Sub - Laboratorio: Wifi 2.4 GHz"); 
       wifi_24_ghz(variable_0, variable_1);
     }
-    else if (!subLab and iniLab)
-    {
-      Serial.println("Sub - Laboratorio: Radio definida por Software");  
-      rad_def_soft(variable_0, variable_1, variable_2, variable_3);
-    }
-  }
-  else
-  {
-    Serial.println("Laboratorio incorrecto");    
-  }
-//  get_json(client);
-}
-
-void wifi_24_ghz(int azimut, int elevacion){
-    if (0 <= azimut and azimut <= 180) 
-    {
-      Serial.println("Azimut:");
-      Serial.println(azimut);
-      servo_azimut.write(azimut);  // Desplazamos a la posición deseada
-      delay(1000);
-    }
-    else
-      Serial.println("El valor de azimut no es correcto. Debe ser entre 0 y 180º");
-    if (0 <= elevacion and elevacion <= 90) 
-    {
-      Serial.println("Elevación:");
-      Serial.println(elevacion);
-      servo_elevacion.write(elevacion);  // Desplazamos a la posición deseada
-      delay(1000);
-    }    
-    else
-      Serial.println("El valor de elevacion no es correcto. Debe ser entre 0 y 90º");
-}
-
-void rad_def_soft(int intensidad_max, int intensidad_min, int modulacion, int codificacion){
-/*  if(iniLab){
-    switch (modulacion)
-    {
-    case 1:
-      Serial.println("Modulación: 4-QAM");
-      break;
-    case 2:
-      Serial.println("Modulación: 8-QAM");
-      break;
-    case 3:
-      Serial.println("Modulación: 16-QAM");
-      break;
-    case 4:
-      Serial.println("Modulación: PSK");
-      break;
-    case 5:
-      Serial.println("Modulación: FSK");
-      break;
-    case 6:
-      Serial.println("Modulación: QPSK");
-      break;          
-    default:
-      Serial.println("Modulación: PSK");
-      break;
+    else{
+      if(bandera_rep==0){
+        Serial.println("Laboratorio Parado");
+        bandera_rep=1;
+      }
     }
   }
   else{
-    Serial.println("Esperando para iniciar el Laboratorio...");
-  }*/
+    if(bandera_rep==0){
+      Serial.println("Laboratorio incorrecto");  
+      bandera_rep = 1; 
+      Errores=3;
+    }
+  }
+}
+
+/**
+ * @brief Funcion utilizada para realizar el movimiento de la antena de a cuerdo al angulo solicitado.
+ * 
+ * @param azimut 
+ * @param elevacion 
+ */
+void wifi_24_ghz(int azimut, int elevacion){
+if (limite_min_azimut <= azimut and azimut <= limite_max_azimut){
+  Serial.println("Azimut:");
+  Serial.println(azimut);
+  servo_azimut.write(azimut);  // Desplazamos a la posición deseada
+  ang_azimut_act = azimut;
+  delay(1000);
+}
+else{
+  Serial.println("El valor de azimut no es correcto. Debe ser entre 0 y 180º");
+  Errores=1;
+} 
+if (limite_min_elevacion <= elevacion and elevacion <= limite_max_elevacion){
+  Serial.println("Elevación:");
+  Serial.println(elevacion);
+  servo_elevacion.write(elevacion);  // Desplazamos a la posición deseada
+  ang_elevacion_act = elevacion;
+  delay(1000);
+}    
+else{
+  Serial.println("El valor de elevacion no es correcto. Debe ser entre 0 y 90º");
+  Errores=2;
+}
 }
--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Protocolo para los laboratorios de "Telecomunicaciones"
+El procedimiento de comunicación es el siguiente.
+Cada vez que se quiere modificar algun dato en el laboratorio. El Servidor debe enviar un
 
-### Json a enviar
+    POST /HTTP/1.1 + <JSON a enviar>
 
-    {"Estado": [ 0, true, false], "Analogicos": [180, 90, 15,52]}
+#### Json a enviar (ejemplo)
+ 
+    { "Estado": [0,true,false],"Analogico": [45,90]}
+
+Y para obtener avances de los datos o reporte de los datos debe enviar un:
+
+      GET /HTTP/1.1
+
+De esta forma el Arduino responde ante una petición.
+
+#### Json a recibir (ejemplo)
+ 
+    { "Estado": [0,true,false],"Analogico": [50,85], "Error": 0}
+
 
 ## Sintaxis
 
@@ -13,20 +28,27 @@ Es un array conformado por 3 elementos en el siguiente orden: [Laboratorio, SubL
 |-|-----|-----|-----------------------------|--------|
 |2|true |true |Telecomunicaciones: Wifi 2.4G|Inicia  |
 |2|true |false|Telecomunicaciones: Wifi 2.4G|Finaliza|
-|2|false|true |Telecomunicaciones: Enlace de Radio por software |Inicia  |
-|2|false|false|Telecomunicaciones: Enlace de Radio por software |Finaliza|
+**Error**
+Es una variable numerica que representa un mensaje de error.
+
+| Tipo de error                         |  Valor  |
+| ----------------------------------    |---------|
+| Sin errores                           |    0    |
+| Error limites de angulo de azimut.    |    1    |
+| Error limites de angulo de elevación  |    2    |
+| Error de laboratorio incorrecto       |    3    |
 **Elementos por Laboratorio**
 
 ***WIFI 2.4***
 
 Se utilizan 2 servo motores uno para cambio de angulo de azimut y otro para el angulo de elevación.
 
-  - Variable a utilizar:
-    - Analogico: [azimut, elevación]
-      - Azimut: 0 - 180 [deg]
-      - Elevación: 0 - 90 [deg]
+- Variable a utilizar:
+  - Analogico: [azimut, elevación]
+    - Azimut: 0 - 180 [deg]
+    - Elevación: 0 - 90 [deg]
 
-***Enlace de Radio por software***
+***Enlace de Radio por software (A definir)***
 
 Se utiliza los parametros elegidos por el usuario en base a las siguientes tablas.
 
